0b262618ed9704bf7582fd521531dd6e2cd8a10a
M98abas <mahmoud.ud@gmail.com>
2022-05-13 00:09:30 +0300

    Lecture-5

---
+++ lect-5/script.js
@@ -0,0 +1,38 @@
+console.log("Hello world");
+console.log(1 + 1);
+console.log(1 - 1);
+console.log(1 * 10);
+console.log(1 / 10);
+console.log(10 % 3);
+console.log(10 ** 3);
+console.log("Hello there", 2, "This Awsome", 2);
+
+let x = 10;
+x *= 10;
+x;
+// x -=1
+x++;
+x;
+x--;
+x;
+
+var z = 30;
+
+const y = 10;
+
+console.log(typeof false);
+
+// Hello team this section is for increment
+/**
+ * jhjnf
+ * markdown
+ */
+
+console.log(20 > 15);
+console.log(15 < 20);
+console.log(15 == 15);
+console.log(15 != 5);
+console.log(15 >= 15);
+console.log(15 === 15);
+
+// less is more
