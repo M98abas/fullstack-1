312ccb27901939346d25713ca0f678fe13283163
M98abas <mahmoud.ud@gmail.com>
2022-05-21 01:26:36 +0300

    finished

---
+++ lect-7/script.js
@@ -0,0 +1,171 @@
+// // let x = 20
+
+// // if (x <= 10)
+// //   console.log("Yes")
+// // else
+// //   console.log("No")
+
+// // for(let i =0; i <= 10; i++)
+
+// // {
+// //   // Syntac
+// //   console.log(i)
+// //   console.log(i+1)
+// // }
+
+// // while (x >= 10)
+// // {
+// // console.log("Hi")
+// //   x--;
+// // }
+
+// // Date
+// /*let dt = new Date()
+// console.log(dt.getFullYear() - 1978)
+// console.log(dt.getDay())
+// // 0=> sun, 1 => mon , 2=>Tue, 3=>Wed, 4=>Thu, 5=>Fri
+// console.log(dt.getDate())
+// console.log(dt.getHours())
+// console.log(dt.getMinutes())
+// console.log(dt.getMilliseconds())
+// */
+
+// // Arrays
+// // datatype stroe multiple data
+// // let x = new Array()
+// // // int arr[10];
+// // let xarr = [1,2,3,4,8,"Hello there this is my array"]
+// // xarr[3]= "Hi"
+// // console.log(xarr)
+// // // Dumy way
+// // // xarr[4]= 1
+// // // xarr[5]= 2
+// // // add
+// // // xarr.push(24) // add to back
+// // // xarr.push(2)
+// // // xarr.push(4)
+// // // // Unshift
+// // // xarr.unshift(120);
+// // console.log(xarr)
+// // // xarr.shift();
+// // // xarr.splice(0,4);
+// // // xarr.pop();
+
+// // console.log(xarr)
+
+// /*
+// let oddArr[5],EvenArr[5]
+
+// random(0..20) is odd or even
+// */
+// // Brain storm
+// // declare the arrays
+// // let oddArr = []
+// // let EvenArr = []
+// // // loop 0..5
+// // for (let i=0; i<10; i++)
+// // {
+// //   // Genrate random Integer number 0..100
+// //   rdm = Math.floor(100* Math.random())
+// //   // check if the number is ordd or even
+// //   if ( rdm % 2 == 0)
+// //     {EvenArr.push(rdm)}
+// //   // if odd add to oddArr else evenArr
+// //   else
+// //     oddArr.push(rdm)
+// // }
+// // console.log(oddArr);
+// // console.log(EvenArr);
+
+// // let arr = []
+// // arr.length = 5
+// // for (let i = 0; i < 20; i++)
+// // arr.push(i)
+
+// // console.log(arr.length);
+// // // arr.length = 3
+// // console.log(arr);
+
+// let arr =[1,2,3,4,5,6,7,8,9,10,11]
+// // for (let i=0;i<arr.length;i++)
+// // {
+// //   if (arr[i] <= 5)
+// //     console.log(arr[i]);
+// // }
+// // for (let element of arr)
+// //   console.log(element);
+
+// arr.map(e=> {
+//   console.log(e)
+// }
+// )
+
+// arrays => indexed array
+// Objects => Key:value
+let obj = {
+  id: 1,
+  name: "Ali",
+  age: 22,
+  // "colors":["red","green","blue"],
+  // "friends":[{"id":1, "name":"HO"}]
+};
+obj.stratus = "New";
+obj.id = 5;
+obj.name = "HAHAHA";
+obj.age = 90;
+console.log(obj);
+// console.log(obj.colors);
+// console.log(obj.friends);
+
+// // let x = [21,4,4]
+// let data = [
+//   {
+//     "id": 1,
+//     "name": "Ali",
+//     "age": 22
+//   },
+//   {
+//     "id": 2,
+//     "name": "Ali",
+//     "age": 22
+//   },
+//   {
+//     "id": 3,
+//     "name": "Ali",
+//     "age": 22
+//   },
+
+//   {
+//     "id": 4,
+//     "name": "Ali",
+//     "age": 22
+//   },
+
+//   {
+//     "id": 5,
+//     "name": "Ali",
+//     "age": 22
+//   },
+
+//   {
+//     "id": 6,
+//     "name": "Ali",
+//     "age": 22
+//   }
+// ]
+// for(let dt of data)
+// {
+//   if (dt.id<=3)
+//   console.log(dt);
+// }
+function hello() {
+  console.log("Hello team");
+}
+hello();
+// Function
+// => Arrow functions
+// => Callback
+// => Promise
+
+// Clean code
+// coders at work
