3b4ee67264965a3b88d60a144abc0b9e4618d01d
M98abas <mahmoud.ud@gmail.com>
2022-05-25 20:19:32 +0300

    Lesson 7

---
+++ lect-8/scripts.js
@@ -0,0 +1,115 @@
+
+// Functions
+// function loging(rt, diout){
+//   let rant = rt
+//   let discount = diout
+//   let x = (2000 - rant)/discount  
+//   console.log(x);
+// }
+
+// loging(12,10)
+// loging(40,100)
+
+// rant = 120
+// discount = 50
+// x = (2000 - rant)/discount
+// x
+
+
+
+// Not Pure 
+// userName = "Ali";
+
+// function outputShow(){
+//   console.log(userName);
+// }
+
+// Pure
+// function outPutShow(name){
+//   console.log(name);
+// }
+
+// outPutShow(userName)
+
+
+// // Factorial   
+// function userAndSalary(name,salary){
+//   return(`User name ${name} and your salary ${salary * 10}`);
+// }
+
+// console.log(userAndSalary("Ali",22));
+// // console.log(UserName,salary*10); 
+
+
+
+// function factorial(x){
+//   if (x == 0) return 1
+//   fact= 1;
+//   for (let i = 1; i <= x; i++){
+//     fact *= i;
+//   }
+//   return fact;
+// }
+
+// // Arrow function
+// const factorial = (x) => {
+//   if (x == 0) return 1
+//   fact= 1;
+//   for (let i = 1; i <= x; i++){
+//     fact *= i;
+//   }
+//   return fact;
+// }
+// console.log(factorial(0));
+
+
+
+
+// function printing(){
+//   return function(){
+//     return "Hello";
+//   }
+// }
+
+// let val = printing()
+// console.log(`val`); 
+
+
+
+// let arr2 = [1,2,3,4,5,6,7]
+// arr.fill(1,2,5)
+// console.log(
+//   arr.map(
+//     (val)=> {
+//     let vl = val / 2
+//     return vl}
+//     )
+// );
+
+// arr.filter(
+//   (val)=>{
+//     return val % 2 == 1
+//   }
+// )
+
+// console.log(
+//   arr.reduce(function(prv,curr){
+//     return prv - curr
+//   })
+//   );
+// console.log(arr);
+// console.log(
+//   arr.concat(arr2)
+// );
+const arr = [1,2,3,4,5,6,7,8,20];
+arr.forEach((val) => console.log(val));
+
+
+
+// console.count()
+
+// 8. Callback, Promise, Big O, Datastrctures, Algorithms 
+// 9. Class & DOM
+// 10. React & SCSS
+// 11. API & React HOOks
+// 12. Nextjs
